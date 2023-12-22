50320f8c2dffe0031b29c5943fd596d818c2d521
lemonshelter <lemonshelter5@gmail.com>
2023-12-22 13:55:42 +0900

    pitch_bend_valを整数へ切り捨て

--- MIDI_preprocess.py
+++ MIDI_preprocess.py
@@ -1,9 +1,10 @@
 import keyboard
+from math import floor
 class MIDI_Preprocess:
     def __init__(self):
         self.upper_lim_of_hand = 100
         self.lower_lim_of_hand = 0
-        self.octave_flag = 0
+        self.octave_flag = 1
         self.hand_position = 0
     
     def set_upper_lim_of_hand(self):
@@ -24,7 +25,7 @@ class MIDI_Preprocess:
     def convet2rootpitch_and_pitchbend(self):
         # 遊びを含めてA#~Dまでの何番目か
         nth_root = self.hand_position // 4096
-        pitch_bend_val = self.hand_position - nth_root*4096
+        pitch_bend_val = floor(self.hand_position - nth_root*4096)
         if self.octave_flag == 0:  # 音量オフ
             root_pitch = 0
             pitch_bend_val = 0
@@ -40,9 +41,10 @@ class MIDI_Preprocess:
 if __name__ == "__main__":
     midi_shori = MIDI_Preprocess()
     midi_shori.bind_keys()
-    midi_shori.set_lower_lim_of_hand()
-    midi_shori.set_upper_lim_of_hand()
-    midi_shori.set_upper_lim_of_hand()
+    # midi_shori.set_lower_lim_of_hand()
+    # midi_shori.set_upper_lim_of_hand()
+    # midi_shori.set_upper_lim_of_hand()
     midi_shori.set_octave(2)
     midi_shori.distance2hand_position(126)
     root_pitch, pitch_bend_val = midi_shori.convet2rootpitch_and_pitchbend()
+    print(f"{root_pitch=}, {pitch_bend_val=}")
