693e940cb788c63c4850138147e7f5b02e50590e
lemonshelter <lemonshelter5@gmail.com>
2024-01-10 22:19:50 +0900

    音量を定数化

--- MIDI2sound.py
+++ MIDI2sound.py
@@ -1,6 +1,8 @@
 import pygame.midi
 import time
 
+VELOCITY = 127
+
 class MIDI2sound:
     def __init__(self):
         # MIDIデバイスの初期化
@@ -11,15 +13,15 @@ class MIDI2sound:
         self.midi_out = pygame.midi.Output(self.device_id)
 
         # トロンボーンの音色に設定
-        self.midi_out.set_instrument(57) 
+        self.midi_out.set_instrument(57)
 
     #音を鳴らす
     def play_note(self, root_pitch, pitch_bend_val):
         self.midi_out.pitch_bend(pitch_bend_val)  # ピッチベンドの値を設定
-        self.midi_out.note_on(root_pitch, 127)  # 音を鳴らす（ピッチ、ベロシティ）
+        self.midi_out.note_on(root_pitch, VELOCITY)  # 音を鳴らす（ピッチ、ベロシティ）
     #音を止める
-    def stop_note(self,root_pitch):
-        self.midi_out.note_off(root_pitch,0)
+    def stop_note(self, root_pitch):
+        self.midi_out.note_off(root_pitch, 0)
 
 # Pygameの終了
 pygame.midi.quit()
